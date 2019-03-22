da6fcfdc7e5c9315df5b46a83c69046043ea2aa2
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-21 23:16:36 -0300

    implementando alterações da aula 23

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,25 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  }
   render() {
     const {name, email, phone} = this.props.contact;
 
     return(
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}
+          <i className="fas fa-sort-down" style={{margin: '0 5px'}} onClick={event => {this.setState({showContactInfo: !this.state.showContactInfo})}}></i>
+        </h4>
+        {this.state.showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>) :
+          null
+        }
       </div>
     )
   }
