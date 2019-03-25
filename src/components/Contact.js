b6656a0200cb39f780812e33fd21a2cad4674528
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-25 09:56:05 -0300

    Criando o bind do clique do botao de excluir o contato para passar a usar o dispatch que esta no global state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,46 +9,50 @@ class Contact extends Component {
     showContactInfo: false
   }
 
-  onDeleteClick = () => {
-    // this.props.deleteHandler()
-    console.log('Falta criar o reducer com o método que manuseará o global state');
-  }
+  onDeleteClick = (id, dispatch) => dispatch({type: 'DELETE_CONTACT', payload: id})
 
   render() {
     const {id, name, email, phone} = this.props.contact;
 
     //TODO: refatorar estilo inline por css apartado
     return (
-      <div className="card card-body mb-3">
-        <h4 className="my-0">
-          {name}
-          <i className="fas fa-sort-down"
-            style={{margin: '0 5px',cursor: 'pointer'}}
-            onClick={event => {
-                this.setState({showContactInfo: !this.state.showContactInfo})
-                if(!this.state.showContactInfo) {event.target.style.transform = 'rotate(180deg) translateY(-50%)'}else{event.target.style.transform = ''}
+      <Consumer>
+        {value => {
+          const { dispatch } = value
+
+          return (
+            <div className="card card-body mb-3">
+              <h4 className="my-0">
+                {name}
+                <i className="fas fa-sort-down"
+                  style={{margin: '0 5px',cursor: 'pointer'}}
+                  onClick={event => {
+                      this.setState({showContactInfo: !this.state.showContactInfo})
+                      if(!this.state.showContactInfo) {event.target.style.transform = 'rotate(180deg) translateY(-50%)'}else{event.target.style.transform = ''}
+                    }
+                }></i>
+                <i className="fas fa-times text-danger align-middle float-right"  style={{cursor: 'pointer'}} onClick={this.onDeleteClick.bind(this, id, dispatch)}></i>
+              </h4>
+              {this.state.showContactInfo ?
+                (
+                  <ul className="list-group">
+                    <li className="list-group-item">Email: {email}</li>
+                    <li className="list-group-item">Phone: {phone}</li>
+                  </ul>
+                )
+              :
+                null
               }
-          }></i>
-          <i className="fas fa-times text-danger align-middle float-right"  style={{cursor: 'pointer'}} onClick={this.onDeleteClick}></i>
-        </h4>
-        {this.state.showContactInfo ?
-          (
-            <ul className="list-group">
-              <li className="list-group-item">Email: {email}</li>
-              <li className="list-group-item">Phone: {phone}</li>
-            </ul>
+            </div>
           )
-        :
-          null
-        }
-      </div>
+        }}
+      </Consumer>
     )
   }
 }
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired, 
-  // deleteHandler: PropTypes.func.isRequired
 }
 
 export default Contact;
