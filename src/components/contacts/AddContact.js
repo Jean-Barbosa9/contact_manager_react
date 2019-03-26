e716f9e6fa191953229a2696cff20ce3946df992
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-26 07:11:11 -0300

    Implementado a manipulaçao do formulario com o state

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -4,49 +4,78 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    showForm: false
+  }
+
+  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
   }
 
   render() {
     const { name, email, phone } = this.state
     return (
       <div className='card mb-3'>
-        <div className="card-header">Add Contact</div>
-        <div className="card-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input 
-                type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter name..."
-                value={name}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input 
-                type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter name..."
-                value={name}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input 
-                type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter name..."
-                value={name}
-              />
-            </div>
-            <input type="submit" className="btn btn-light btn-block" value="Add Contact"/>
-          </form>
+        <div 
+          className="card-header"
+          style={{margin: '0 5px',cursor: 'pointer'}}
+          onClick={event => {
+              this.setState({showForm: !this.state.showForm})
+              /*
+                if(!this.state.showForm) {event.target.style.transform = 'rotate(180deg) translateY(-50%)'}else{event.target.style.transform = ''}
+                aguardando para aprender em como mexer nos nós filhos, pois a ideia é rotacionar apenas o ícone
+              */
+            }
+          }
+        >
+          Add Contact
+          <i className="fas fa-sort-down float-right"></i>
         </div>
+        {this.state.showForm ? (
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Name: </label>
+                <input 
+                  type="text"
+                  name="name"
+                  className="form-control form-control-lg"
+                  placeholder="Enter name..."
+                  value={name}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                  <label htmlFor="emai">Email: </label>
+                <input 
+                  type="text"
+                  name="email"
+                  className="form-control form-control-lg"
+                  placeholder="Enter email..."
+                  value={email}
+                  onChange={this.onChange}                
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone: </label>
+                <input 
+                  type="text"
+                  name="phone"
+                  className="form-control form-control-lg"
+                  placeholder="Enter phone..."
+                  value={phone}
+                  onChange={this.onChange}
+                />
+              </div>
+              <input type="submit" className="btn btn-light btn-block" value="Add Contact"/>
+            </form>
+          </div>
+        )
+        :
+        null}
+        
       </div>
       )
     }
