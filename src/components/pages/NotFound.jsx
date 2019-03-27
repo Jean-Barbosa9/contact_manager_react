a42aa9c4c3e45cfbf662f0323e98146bb1f80d3c
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 08:23:24 -0300

    Criando página não encontrada

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="display-4"><span style={{color: '#61DAFB'}}>404</span>Not Found</h1>
+      <p className="lead">Sorry, this page doesn't exist!</p>
+    </div>
+  )
+}
