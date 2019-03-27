5853e77f3dfb2c48bfc4a2bea5b55a008ca0ccd5
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 08:09:11 -0300

    Personalizando a mensagem em caso de parâmetro

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { version } from '../../../package.json'
 
-export default function About() {
+export default function About(props) {
+  const name = props.match.params.name
   return (
     <div>
       <h1 className="display-4">About Contact Manager</h1>
-      <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">Version: {version}</p>
+      {name ? (
+        <React.Fragment>
+          <p className="lead">Hello {name}, it's a simple app to manage contacts</p>
+          <p className="text-secondary">Version: {version}</p>  
+        </React.Fragment>
+        ):(
+        <React.Fragment>
+          <p className="lead">Simple app to manage contacts</p>
+          <p className="text-secondary">Version: {version}</p>
+        </React.Fragment>
+      )}
     </div>
   )
 }
