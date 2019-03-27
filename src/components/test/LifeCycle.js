8a2b0d2173043bcb706cce934dd4f0afc308f9a6
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 14:01:26 -0300

    Usando fetch para popular html

--- src/components/test/LifeCycle.js
+++ src/components/test/LifeCycle.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 
 class LifeCycle extends Component {
   state = {
-    description: 'Component used as reference to life cycle methods notes.'
+    description: 'Component used as reference to life cycle methods notes.',
+    title: '',
+    body: ''
   }
 
   componentDidMount () {
     console.log("componentDidMount will fires after the actual component had fished to be mounted");
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({title: data.title, body: data.body}))
   }
 
   // UNSAFE_componentWillMount () {
@@ -37,7 +42,7 @@ class LifeCycle extends Component {
   render() {
     return (
       <div>
-        <h1 className="display-4">React life cycle methods</h1>
+        <h1 className="display-3">React life cycle methods</h1>
         <p className="lead">
           Read more about each one of them at:
           <a href="https://medium.com/@baphemot/understanding-react-react-16-3-component-life-cycle-23129bc7a705">Understanding life cycle methods React 16.3</a>, <a href="https://reactjs.org/">React docs</a>
@@ -46,6 +51,9 @@ class LifeCycle extends Component {
           <br/>
           Press <strong>ctrl+shift+i</strong> to see it's summed up into console log.
         </p>
+        <h2 className="display-4">componentDidMount sample</h2>
+        <h3>{this.state.title}</h3>
+        <p>{this.state.body}</p>
       </div>
     )
   }
