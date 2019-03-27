c833c8365a702e52032f7323b0b25219c4dae0f8
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 07:28:45 -0300

    Adicionando um título

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -12,6 +12,7 @@ class Contacts extends Component {
           const { contacts } = value
           return (
             <React.Fragment>
+              <h1 className="display-4 mb-3"><span style={{color: '#61DAFB'}}>Contacts</span> List</h1>
               {contacts.map(contact => (
                 <Contact 
                   key={contact.id}
