bfe16c1ca7075ecd3531550e0c3f87e5f65bf0a8
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 08:08:50 -0300

    Adicionando mais links para as páginas criadas

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,13 +8,28 @@ const Header = props => {
   const {branding} = props;
 
   return (
-    <header className="header ">
+    <header className="header">
       <nav className="menu navbar navbar-expand-sm navbar-dark mb-3 py-0" style={{backgroundColor: '#61DAFB'}}>
         <div className="container">
           <a className="menu__link navbar-brand" style={{color: '#000000'}} href="/">{branding}</a>
-          <ul className="menu__list navbar-nav mr-auto">
+          <ul className="menu__list navbar-nav">
             <li className="menu__item nav-item">
-              <a className="menu__link nav-link" style={{color: '#000000'}} href="/">Home</a>
+              <Link to="/" className="menu__link nav-link" style={{color: '#000000'}}>
+                <i className="fas fa-home"></i>
+                Home
+              </Link>
+            </li>
+            <li className="menu__item nav-item">
+              <Link to="/contact/add" className="menu__link nav-link" style={{color: '#000000'}}>
+                <i className="fas fa-plus"></i>
+                Add
+              </Link>
+            </li>
+            <li className="menu__item nav-item">
+              <Link to="/about" className="menu__link nav-link" style={{color: '#000000'}}>
+                <i className="fas fa-question"></i>
+                About
+              </Link>
             </li>
           </ul>
         </div>
