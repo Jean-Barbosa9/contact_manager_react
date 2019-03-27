435292898f3f6691ac782b7ede35f19ba9b082e5
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 07:25:21 -0300

    Alterando a estilização do header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -8,12 +8,12 @@ const Header = props => {
 
   return (
     <header className="header ">
-      <nav className="menu navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <nav className="menu navbar navbar-expand-sm navbar-dark mb-3 py-0" style={{backgroundColor: '#61DAFB'}}>
         <div className="container">
-          <a className="menu__link navbar-brand" href="/">{branding}</a>
+          <a className="menu__link navbar-brand" style={{color: '#000000'}} href="/">{branding}</a>
           <ul className="menu__list navbar-nav mr-auto">
             <li className="menu__item nav-item">
-              <a className="menu__link nav-link" href="/">Home</a>
+              <a className="menu__link nav-link" style={{color: '#000000'}} href="/">Home</a>
             </li>
           </ul>
         </div>
