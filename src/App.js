9702ba92401228b59c491801ced7232c2e4321bd
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 08:23:12 -0300

    Adicionando mais uma rota

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from './context';
 import Header from './components/layouts/Header';
 import AddContact from './components/contacts/AddContact';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -21,6 +22,7 @@ class App extends Component {
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about/" component={About} />
                 <Route exact path="/about/:name" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
