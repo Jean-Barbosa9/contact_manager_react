8800af00326916bdd07eb9d6aa6b6bd189adfcab
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-27 07:23:44 -0300

    Adicionando a página sobre

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { version } from '../../../package.json'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple app to manage contacts</p>
+      <p className="text-secondary">Version: {version}</p>
+    </div>
+  )
+}
