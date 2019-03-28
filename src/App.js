860827da4695bb0e1932a83dd5634384e22351e0
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-28 08:46:57 -0300

    Importando EditContacts e criando rota pra ele

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from './components/contacts/Contacts';
 import { Provider } from './context';
 import Header from './components/layouts/Header';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import About from './components/pages/About';
 import LifeCycle from './components/test/LifeCycle';
 import NotFound from './components/pages/NotFound';
@@ -21,6 +22,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about/" component={About} />
                 <Route exact path="/about/:name" component={About} />
                 <Route exact path="/life-cycle-refs" component={LifeCycle} />
