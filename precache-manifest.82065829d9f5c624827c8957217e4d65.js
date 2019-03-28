65469a26a00c7a21667490101e755c2cf2a2dbf2
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-03-28 13:37:31 -0300

    Updates

---
+++ precache-manifest.82065829d9f5c624827c8957217e4d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "826866c234e74579a79c",
+    "url": "/contact_manager_react/static/js/runtime~main.6fb8c003.js"
+  },
+  {
+    "revision": "781a5c44ebcf3b64535f",
+    "url": "/contact_manager_react/static/js/main.25bb248a.chunk.js"
+  },
+  {
+    "revision": "670914768d9a049f826c",
+    "url": "/contact_manager_react/static/js/2.c5911e92.chunk.js"
+  },
+  {
+    "revision": "781a5c44ebcf3b64535f",
+    "url": "/contact_manager_react/static/css/main.0666757d.chunk.css"
+  },
+  {
+    "revision": "670914768d9a049f826c",
+    "url": "/contact_manager_react/static/css/2.38473f94.chunk.css"
+  },
+  {
+    "revision": "f3aac2a9e69c7bcee2bbc763ad3c28a0",
+    "url": "/contact_manager_react/index.html"
+  }
+];
